0851c2abd83e4e809e62720ca6bec5e50754dfef
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-12-06 17:31:59 -0500

    ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+
   env:{
     schema:{
       SHOW_BUY_BUTTON: envField.boolean({context: 'server', access: 'public'}),
       SCORE_API_ENDPOINT: envField.string({context: 'server', access: 'public'})
     }
-  }
+  },
+
+  adapter: vercel()
 });
