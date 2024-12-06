670d2d7619f3312245f0570dc6f875b36a50b090
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-12-06 12:43:46 -0500

    working with env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  env:{
+    schema:{
+      SHOW_BUY_BUTTON: envField.boolean({context: 'server', access: 'public'}),
+      SCORE_API_ENDPOINT: envField.string({context: 'client', access: 'public'})
+    }
+  }
 });
